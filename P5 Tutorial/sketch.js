78e9c8dfbb32b4ea935df5be7b4407b00e9559bb
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-08-19 22:56:35 -0700

    work with static objects

--- P5 Tutorial/sketch.js
+++ P5 Tutorial/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas(400, 400);
+  
 }
 
 function draw() {
   background(220);
+  fill('aquamarine')
+  stroke('orange')
+  strokeWeight(5)
+  circle (100, 100, 100)
+  textSize(50)
+  text("💮", 94, 105)
+  text("🐞", mouseX, mouseY)
 }
